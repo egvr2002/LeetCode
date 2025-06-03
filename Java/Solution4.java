6cdcf53e41e9c637c0d39dfca729aad3764276fb
Leonardo Trevizo <leonardo.trevizo@encora.com>
2025-06-03 16:33:54 -0600

    Java folder created
    java files for the solutions were created and appended into the java folder

---
+++ Java/Solution4.java
@@ -0,0 +1,24 @@
+package Java;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution4 {
+    public String shortestPalindrome(String s){
+
+        //function shortestPalindrome(s: string): string {
+        //    const t = s.split('').reverse().join('');
+        //
+        //    for (let i = 0; i < t.length; i++) {
+        //      if (s.startsWith(t.slice(i))) {
+        //        return t.slice(0, i) + s;
+        //      }
+        //    }
+        //
+        //    return t + s;
+        //};
+        //String[] t = s.split("");
+        return "hello";
+    }
+}
