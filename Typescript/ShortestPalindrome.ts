94cce716921d600eb755b0624f36c0bf6b5e4210
Francisco Castillo <francisco.castillo@encora.com>
2025-06-03 10:09:35 -0600

    feat: added Shortest Palindrome TS

---
+++ Typescript/ShortestPalindrome.ts
@@ -0,0 +1,11 @@
+function shortestPalindrome(s: string): string {
+    const t = s.split('').reverse().join('');
+
+    for (let i = 0; i < t.length; i++) {
+      if (s.startsWith(t.slice(i))) {
+        return t.slice(0, i) + s;
+      }
+    }
+
+    return t + s;
+};
