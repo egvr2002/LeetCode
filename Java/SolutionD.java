07cb0fb7b879b2d1a983ab9ece296c5dc057a6a6
Leonardo Trevizo <leonardo.trevizo@encora.com>
2025-06-04 20:42:54 -0600

    Updated SolutionD.java
    Uploaded solution created during a teams call

--- Java/SolutionD.java
+++ Java/SolutionD.java
@@ -1,23 +1,13 @@
-package Java;
-
 public class SolutionD {
-    public String shortestPalindrome(String s){
+    public String shortestPalindrome(String s) {
+        String t = new StringBuilder(s).reverse().toString();
 
-        //function shortestPalindrome(s: string): string {
-        //    const t = s.split('').reverse().join('');
-        //
-        //    for (let i = 0; i < t.length; i++) {
-        //      if (s.startsWith(t.slice(i))) {
-        //        return t.slice(0, i) + s;
-        //      }
-        //    }
-        //https://leetcode.com/problems/shortest-palindrome/description/
-        //https://leetcode.com/problems/patching-array/
-        //https://leetcode.com/problems/department-top-three-salaries/description/
-        //https://leetcode.com/problems/shortest-palindrome/description/
-        //    return t + s;
-        //};
-        //String[] t = s.split("");
-        return "hello";
+        for (int i = 0; i < t.length(); i++) {
+            if (s.startsWith(t.substring(i))) {
+                return t.substring(0, i) + s;
+            }
+        }
+
+        return t + s;
     }
 }
