131e498b224307f6b445ffaead170788bc697f9e
Leonardo Trevizo <leonardo.trevizo@encora.com>
2025-06-06 17:19:20 -0600

    Uploaded meetings codes Upload the solution for each problem obtained from the video call on Teams.

--- Java/SolutionA.java
+++ Java/SolutionA.java
@@ -1,4 +1,41 @@
 package Java;
 
 public class SolutionA {
+    //
+    //class Solution:
+    //def maxProfit(self, prices: List[int]) -> int:
+    // sellTwo = 0
+    // holdTwo = -math.inf
+    // sellOne = 0
+    // holdOne = -math.inf
+
+    // for price in prices:
+    //    sellTwo = max(sellTwo, holdTwo + price)
+    //    holdTwo = max(holdTwo, sellOne - price)
+    //    sellOne = max(sellOne, holdOne + price)
+    //    holdOne = max(holdOne , -price)
+
+    // return sellTwo
+    //
+    //
+    //
+    //
+    //
+    //
+    //class Solution:
+    //def maxProfit(self, prices: List[int]) -> int:
+    // sellTwo = 0
+    // holdTwo = math.inf
+    // sellOne = 0
+    // holdOne = math.inf
+
+    // for price in prices:
+    //    sellTwo = min(sellTwo, holdTwo - price)
+    //    holdTwo = min(holdTwo, sellOne + price)
+    //    sellOne = min(sellOne, holdOne - price)
+    //    holdOne = min(holdOne , price)
+
+    // return -sellTwo
+
+    //
 }
--- Java/SolutionB.java
+++ Java/SolutionB.java
@@ -1,4 +1,21 @@
 package Java;
 
 public class SolutionB {
+    //
+    // class Solution:
+    //def minPatches(self, nums: List[int], n: int) -> int:
+    //    patches = 0
+    //    min_num = 1
+    //    i = 0
+    //    while (min_num <= n):
+    //        if i < len(nums) and nums[i] <= min_num:
+    //            min_num += nums[i]
+    //            i += 1
+    //        else:
+    //            patches += 1
+    //            min_num *= 2
+    //
+    //    return patches
+    //
+    //
 }
